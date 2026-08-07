3c438f23d7f60d24b5125f8f2daa6f11fdebad3f
Blackford Joe <Joe.Blackford@glos.nhs.uk>
2026-08-07 14:33:21 +0100

    troubleshooting startup

--- app.R
+++ app.R
@@ -1,8 +1,17 @@
 library(shiny)
-
+message("Loading globals...")
 source("R/globals.R")
+message("Globals loaded")
+
+message("Loading UI...")
 source("R/ui.R")
+message("UI loaded")
+
+message("Loading server...")
 source("R/server.R")
+message("Server loaded")
+
+message("Starting Shiny...")
 
 shinyApp(ui, server)
 
